9612effe672770778d795ccfeb75bb336633028d
Yevhen <shershen.yevhen@gmai>
2022-11-12 21:23:25 +0100

    Сортировка пузырьком (Bubble sort)

---
+++ bubbleSortAlgorithm.js
@@ -0,0 +1,20 @@
+const arr = [
+  1, 21, 3, 5, 6, 2, 21, 313, 55, -2, -54, 231, 12, 222, 11, 0, -11, -21, -2,
+  -54, 231, 12, 222, 11, 0, -11, -21, 1111,
+];
+let count = 0;
+function bubbleSort(array) {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (array[j + 1] < array[j]) {
+        let tmp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = tmp;
+      }
+      count += 1;
+    }
+  }
+  return array;
+}
+console.log(bubbleSort(arr));
+console.log(`Сортировка пузырьком: ${count}`); //O(n*n)
