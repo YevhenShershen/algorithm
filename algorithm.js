d12b718c0a9e47ee7c55afdcc51f8305c827835f
Yevhen <shershen.yevhen@gmai>
2022-11-12 09:51:26 +0100

    Линейный Алгоритм (Linear Search Algorithm)

--- algorithm.js
+++ algorithm.js
@@ -12,18 +12,7 @@
 //O(n2) — квадратичная сложность
 
 //ПРИМЕРЫ АЛГОРИТМОВ
-
 //1)Алгоритм линейного поиска в массиве
-const arr = [1, 4, 5, 8, 9, 1, 2, 7, 5, 2, 11];
-let count = 0;
-function lineArrSearch(array, item) {
-  for (let i = 0; i < array.length; i++) {
-    count += 1;
-    if (arr[i] === item) {
-      return i;
-    }
-  }
-  return "We didn`t find an element in the array";
-}
-console.log(lineArrSearch(arr, 0));
-console.log(`Count: ${count}`);
+//2)Бинарный поиск (Binary search)
+
+
