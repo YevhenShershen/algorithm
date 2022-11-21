b038506357052fffe632e9c6e249a87d91128e13
Yevhen <shershen.yevhen@gmai>
2022-11-21 20:13:49 +0100

    Рекурсивная бинарная сортировка

--- recursionBinarySearch.js
+++ recursionBinarySearch.js
@@ -1,15 +1,15 @@
 
 //Рекурсивная бинарная сортировка
 let arr1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
-function BinarySearch(arr, start, end, key) {
+function binarySearch(arr, start, end, key) {
     if(start > end) return -1;
     let mid = Math.floor((start + end) / 2);
 
     if(arr[mid] === key) return mid;
     if(key > arr[mid]) {
-        return BinarySearch(arr, mid + 1, end, key);
+        return binarySearch(arr, mid + 1, end, key);
     } else if(key < arr[mid]) {
-        return BinarySearch(arr, start, mid -1, key);
+        return binarySearch(arr, start, mid -1, key);
     }
 }
-console.log(BinarySearch(arr1, 0, arr1.length - 1, 2))
+console.log(binarySearch(arr1, 0, arr1.length - 1, 2))
